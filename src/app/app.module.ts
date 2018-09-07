d589c1dc1cc4452e70ed079e51221e5cde6cbf79
shankarganesh1234 <shankar.ganesh.1234@gmail.com>
2018-09-06 22:49:23 -0700

    Added particles and minor ui changes for home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,25 +12,29 @@ import {TinybitGetComponent} from "./tinybit-get/tinybit-get.component";
 import {WebService} from "./services/web.service";
 import {IPFSService} from "./services/ipfs.service";
 import {TinybitAboutComponent} from "./tinybit-about/tinybit-about.component";
+import {ParticlesModule} from 'angular-particle';
+
 
 @NgModule({
-  declarations: [
-      AppComponent,
-      TinybitHomeComponent,
-      TinybitFooterComponent,
-      TinybitCreateComponent,
-      TinybitGetComponent,
-      TinybitAboutComponent
-  ],
-  imports: [BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    HttpModule,
-    JsonpModule,
-    ReactiveFormsModule
-  ],
-  providers: [CoreService, WebService, IPFSService],
-  bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA], // add this!
+    declarations: [
+        AppComponent,
+        TinybitHomeComponent,
+        TinybitFooterComponent,
+        TinybitCreateComponent,
+        TinybitGetComponent,
+        TinybitAboutComponent
+    ],
+    imports: [BrowserModule,
+        FormsModule,
+        AppRoutingModule,
+        HttpModule,
+        JsonpModule,
+        ReactiveFormsModule,
+        ParticlesModule
+    ],
+    providers: [CoreService, WebService, IPFSService],
+    bootstrap: [AppComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA], // add this!
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/tinybit-home/tinybit-home.component.ts
+++ src/app/tinybit-home/tinybit-home.component.ts
@@ -15,6 +15,13 @@ export class TinybitHomeComponent implements OnInit{
 
     isWeb3Supported: boolean  = false;
     count: Counter = new Counter();
+
+    myStyle: object = {};
+    myParams: object = {};
+    width: number = 100;
+    height: number = 100;
+
+
     constructor(private route: ActivatedRoute, private router: Router, private coreService: CoreService, private webService: WebService){
         route.params.subscribe(val => {
 
@@ -26,6 +33,37 @@ export class TinybitHomeComponent implements OnInit{
         this.coreService.getCounter().subscribe(result => {
             this.count = result;
         });
-    }
+
+        this.myStyle = {
+            'position': 'fixed',
+            'width': '100%',
+            'height': '100%',
+            'z-index': -1,
+            'top': 0,
+            'left': 0,
+            'right': 0,
+            'bottom': 0
+        };
+
+        this.myParams = {
+            particles: {
+                number: {
+                    value: 50,
+                },
+                color: {
+                    value: '#000000'
+                },
+                shape: {
+                    type: 'triangle',
+                },
+                line_linked: {
+                    enable: true,
+                    distance: 150,
+                    color: '#000000',
+                    opacity: 0.4,
+                    width: 1
+                }
+            }
+    }}
 }
 
